e896bdae6993a6f790a8b5de814bc1adbc19d1ff
Omar Selaya <selayaomar777@gmail.com>
2024-07-30 13:26:36 -0400

    Organizacion del proyecto

--- react-curso/src/App/index.js
+++ react-curso/src/App/index.js
@@ -1,10 +1,6 @@
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
-
+import { useLocalStorage } from './useLocalStorage'
+import { AppUI } from './AppUI';
 // const defaultTodos = [
 //   {text:'Cortar cebolla', completed: true},
 //   {text:'Curso react.js', completed: false},
@@ -16,26 +12,7 @@ import React from 'react';
 // localStorage.setItem('TODOS_v1', JSON.stringify(defaultTodos))
 // localStorage.removeItem('TODOS_v1')
 
-function useLocalStorage(itemName, initialValue){
-  
-  const localStorageItem = localStorage.getItem(itemName, initialValue);
-  let parsedItem;
-  if(!localStorageItem){
-    localStorage.setItem(itemName, JSON.stringify(initialValue))
-    parsedItem = initialValue; 
-  }else{
-    parsedItem = JSON.parse(localStorageItem);
-  }
-  
-  const [item, setItem] = React.useState(parsedItem)
-  
-  const saveItem = (newItem) =>{
-    localStorage.setItem(itemName, JSON.stringify(newItem))
-    setItem(newItem);
-  }
 
-  return [item, saveItem]
-}
 
 function App() {
   
@@ -75,29 +52,17 @@ function App() {
     saveTodos(newTodos);
   }
 
-  return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos}/>
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={()=> deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </>
-  );
+  return(
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo ={deleteTodo}
+    />
+  )
 }
 
 
